ac3e007a755ef2359f40749285a9c65f84ee9262
medziedbelguith <medziedbelguith@gmail.com>
2022-07-29 11:49:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import CreateUser from './components/create-user.component'
+import Users from './components/users.component'
 
 function App() {
   return (
+    <Router> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a className="navbar-brand">React Axios Tutorial</a>
+          <div
+            className="collapse navbar-collapse"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to={'/create-user'}>
+                  Create User
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={'/users'}>
+                  Users List
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <Routes>
+              <Route exact path="/" element={<CreateUser />} />
+              <Route path="/create-user" element={<CreateUser />} />
+              <Route path="/users" element={<Users />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </div>
+  </Router>
   );
 }
 
